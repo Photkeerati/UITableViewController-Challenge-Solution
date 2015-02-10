090a5653229ac7826d76939451f1aca8a133e451
Photkeerati <pojkeerati@gmail.com>
2015-02-11 01:02:43 +0700

    First Commit

---
+++ UITableViewControllerCharengeSolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewControllerCharengeSolution
+//
+//  Created by Photkeerati on 2/11/2558 BE.
+//  Copyright (c) 2558 Photkeerati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewControllerCharengeSolutionTests/UITableViewControllerCharengeSolutionTests.m
@@ -0,0 +1,40 @@
+//
+//  UITableViewControllerCharengeSolutionTests.m
+//  UITableViewControllerCharengeSolutionTests
+//
+//  Created by Photkeerati on 2/11/2558 BE.
+//  Copyright (c) 2558 Photkeerati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UITableViewControllerCharengeSolutionTests : XCTestCase
+
+@end
+
+@implementation UITableViewControllerCharengeSolutionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
