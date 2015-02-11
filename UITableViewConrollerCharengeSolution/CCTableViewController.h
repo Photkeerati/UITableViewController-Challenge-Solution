aa831454a1f670bf8199d3b1c48b1668c940d946
Photkeerati <pojkeerati@gmail.com>
2015-02-12 01:06:51 +0700

    Updated UI

--- UITableViewConrollerCharengeSolution/CCTableViewController.h
+++ UITableViewConrollerCharengeSolution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CCTableViewController.h
 //  UITableViewConrollerCharengeSolution
 //
 //  Created by Photkeerati on 2/12/2558 BE.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CCTableViewController : UITableViewController
 
 @end
-
