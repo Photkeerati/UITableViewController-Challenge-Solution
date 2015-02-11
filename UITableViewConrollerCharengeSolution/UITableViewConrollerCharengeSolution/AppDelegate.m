6caf39d7685914cc9e89388624280d46b79997e3
Photkeerati <pojkeerati@gmail.com>
2015-02-12 00:26:52 +0700

    Test commit2

--- UITableViewConrollerCharengeSolution/UITableViewConrollerCharengeSolution/AppDelegate.m
+++ UITableViewConrollerCharengeSolution/UITableViewConrollerCharengeSolution/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"Hi");
+    NSLog(@"hi2");
     return YES;
 }
 
